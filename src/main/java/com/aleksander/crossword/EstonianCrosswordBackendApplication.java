79371a0bb2370e14399ad85d02c4542da51cb160
Aleksander Ontin <aleksander.ontin@gmail.com>
2026-04-23 12:03:50 +0300

    first commit

---
+++ src/main/java/com/aleksander/crossword/EstonianCrosswordBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aleksander.crossword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstonianCrosswordBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstonianCrosswordBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aleksander/crossword/EstonianCrosswordBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksander.crossword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstonianCrosswordBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
