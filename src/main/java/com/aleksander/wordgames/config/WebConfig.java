3046bee769d150e257ec981a5eea4871e83c8ac9
Aleksander Ontin <aleksander.ontin@gmail.com>
2026-05-02 22:04:30 +0300

    Add CORS configuration via ConfigurationProperties

--- src/main/java/com/aleksander/wordgames/config/WebConfig.java
+++ src/main/java/com/aleksander/wordgames/config/WebConfig.java
@@ -1,22 +1,31 @@
 package com.aleksander.wordgames.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.micrometer.common.lang.NonNull;
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${cors.allowed-origins}")
-    private String[] allowedOrigins;
+    private final CorsProperties corsProperties;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns(allowedOrigins)
-                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
+                        .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
